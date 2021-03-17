0674395b2caa7dd81933db1233af717ca0ae045a
jmochel <jmochel@truxnow.com>
2021-03-17 12:47:16 -0400

    Initial app check in

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# microservice-exemplar
-Best Practice Exemplar for DW
+# Exemplar
+
+How to start the Exemplar application
+---
+
+1. Run `mvn clean install` to build your application
+1. Start application with `java -jar target/microservice-exemplar-1.0-SNAPSHOT.jar server config.yml`
+1. To check that your application is running enter url `http://localhost:8080`
+
+Health Check
+---
+
+To see your applications health enter url `http://localhost:8081/healthcheck`
---
+++ src/main/java/com/trux/ExemplarApplication.java
@@ -0,0 +1,29 @@
+package com.trux;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ExemplarApplication extends Application<ExemplarConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new ExemplarApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Exemplar";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<ExemplarConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final ExemplarConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
